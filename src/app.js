6cb5a8845b0bf0626a532eaba78e9f64f99a0d33
Michel Martínez <soymichelmtz@hotmail.com>
2026-01-08 09:14:55 -0600

    assets: use assets/couple-plans.svg as app icon/logo

--- src/app.js
+++ src/app.js
@@ -451,7 +451,7 @@ function renderTopbar() {
   return el('header', { className: 'topbar' },
     el('div', { className: 'topbar-inner' },
       el('div', { className: 'brand' },
-        el('div', { className: 'brand-logo', innerHTML: '<img alt="" src="./assets/icon.svg" width="22" height="22" />' }),
+  el('div', { className: 'brand-logo', innerHTML: '<img alt="" src="./assets/couple-plans.svg" width="22" height="22" />' }),
         el('div', { className: 'brand-title' },
           el('strong', { textContent: 'Couple Plans' }),
           el('span', {
